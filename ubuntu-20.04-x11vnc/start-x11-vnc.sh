063a7744d928f24d0aa5fc53eb66b67bc500c385
Thierry Carrard <thierry.carrard@cea.fr>
2026-02-16 20:39:48 +0100

    final version of embedded Xvfb X11 server with vnc/Remmina desktop

--- ubuntu-20.04-x11vnc/start-x11-vnc.sh
+++ ubuntu-20.04-x11vnc/start-x11-vnc.sh
@@ -1,4 +1,4 @@
-VNCDISPLAY=:1
+VNCDISPLAY=:2
 echo "start xvfb on $VNCDISPLAY"
 Xvfb "$VNCDISPLAY" -screen 0 1920x1080x24 &
 sleep 1
@@ -7,6 +7,6 @@ fluxbox -display "$VNCDISPLAY" &
 x11vnc -display "$VNCDISPLAY" -bg -nopw -listen localhost -xkb
 sleep 1
 echo "start VNC client"
-xterm &
+#xterm &
 remmina -c vnc://localhost
 
